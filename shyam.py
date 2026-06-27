5267176bc41f488cab958c2a5187525c4d45659a
shyam <patelshyambhai2@gmail.com>
2026-06-27 13:16:12 +0530

    code.py

---
+++ shyam.py
@@ -0,0 +1,57 @@
+print("hello world")
+print("shyam")
+print("silver oak university")
+print("wellcome to programming")
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print("i love python")
+print("thusday")
+print("data analaysis")
+print("Learning Python is easy")
+print("ahmedabad")
+
+#variable task
+name="shyam"
+print(name)
+age=21
+print(age)
+phone_no = 8320581824
+print("my phone no is",phone_no)
+Email="patelshyambhai2@gmail.com"
+print("my email adddres",Email)
+country="india"
+print("country name",country)
+message="welcome"
+print(message)
+city="ahmdabad"
+print(city)
+college_name="silver oak university"
+print(college_name)
+fev_color="black"
+print("my fev color",fev_color)
+fev_subject="python"
+print(fev_subject)
+
+name = input("Enter your name: ")
+print(name)
+age = input("Enter your age: ")
+print(age)
+city = input("Enter your city name: ")
+print(city)
+college = input("Enter your college name: ")
+print(college)
+color = input("Enter your favorite color: ")
+print(color)
+subject = input("Enter your favorite subject: ")
+print(subject)
+phone_number = input("Enter your phone number: ")
+print(phone_number)
+email = input("Enter your email address: ")
+print(email)
+country = input("Enter your country name: ")
+print(country)
+message = input("Enter a welcome message: ")
+print(message)
